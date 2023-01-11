dce13e0dfef3d9a95f34f939729d87732cc885fa
Max <60371404+iaphub@users.noreply.github.com>
2023-01-10 19:15:05 -0600

    Version updated to 4.1.0

--- Iaphub/IHConfig.swift
+++ Iaphub/IHConfig.swift
@@ -16,7 +16,7 @@ class IHConfig {
    // SDK platform
    static var sdk = "ios"
    // SDK version
-   static var sdkVersion = "4.0.2"
+   static var sdkVersion = "4.1.0"
    // Cache version (Increment it when cache needs to be reset because of a format change)
    static var cacheVersion = "1"
 }
