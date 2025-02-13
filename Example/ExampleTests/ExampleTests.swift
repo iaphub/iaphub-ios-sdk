9d65a9145d586040b7ae7b449f690e587f996fe9
Max <60371404+iaphub@users.noreply.github.com>
2025-02-12 20:26:12 -0600

    Fix test

--- Example/ExampleTests/ExampleTests.swift
+++ Example/ExampleTests/ExampleTests.swift
@@ -88,6 +88,9 @@ class IaphubTests: XCTestCase {
    func test01_getProductsForSale() async throws {
       var userFetched = false
       
+      // Force refresh
+      Iaphub.shared.user?.fetchDate = Date(timeIntervalSince1970: Date().timeIntervalSince1970 - 60 * 60 * 25)
+      
       Iaphub.shared.user?.api?.network.mock = {(type, route, params) in
          if (type == "GET" && route.contains("/user")) {
             userFetched = true
@@ -110,7 +113,7 @@ class IaphubTests: XCTestCase {
       XCTAssertEqual(products[0].localizedPrice, "$1.99")
       XCTAssertEqual(products[0].price, 1.99)
       XCTAssertEqual(products[0].currency, "USD")
-      XCTAssertEqual(userFetched, false)
+      XCTAssertEqual(userFetched, true)
    }
    
    func test02_getUserId() async throws {
